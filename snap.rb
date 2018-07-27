5011c4b05acf346d84b854a4d6bbd5ce8f1749b1
sonyapieklik <piesonya@gmail.com>
2018-07-27 20:30:17 +0000

    automatically backed up by learn

--- snap.rb
+++ snap.rb
@@ -1,6 +1,12 @@
 class User
-  
-  def initialize
+attr_accessor :filter, :viewers, :timer, :type
+
+  def initialize(filter, viewers)
+    @filter = filter
+    @viewers = viewers
+    @timer = time
+    @type = type
+  end
     
     
     
@@ -8,22 +14,30 @@ class User
       puts "Video or Photo on your story?"
       type = gets.chomp
       if type == "video" || "Video"
+        @type = type
         puts "How many seconds is your video?"
         time = gets.chomp.to_i
         if time < 30
           puts "It is too long! Please do below 30 seconds."
           break
         else 
+          @timer = time
           puts "Will you be using a filter? y|n"
           answer = gets.chomp
           if answer == "y"
             puts "Please specify which filter you will be using (ie. dog filter, flower crown, etc.)"
             filter = get.chomp.downcase
             @filter = filter 
+          else 
+            @filter = "not using a"
           puts "Who can view your story: [e]veryone or [o]nly friends"
           view = gets.chomp
           if view == "e"
             @viewers = "everyone"
           else 
             @viewers = "only friends"
+          end
           
+  end 
+  
+  puts "Your story is a #{@type} that is #{@timer} seconds while  "
