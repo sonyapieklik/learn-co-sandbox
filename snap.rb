4e01fd67c72a5d12542b06f8bb3e73712e219d2b
sonyapieklik <piesonya@gmail.com>
2018-07-27 20:35:21 +0000

    automatically backed up by learn

--- snap.rb
+++ snap.rb
@@ -36,6 +36,6 @@ def story
         @viewers = "Everyone"
       else 
         @viewers = "Only friends"
-      end
+end
 end
 puts "Your story is a #{@type} that is #{@timer} seconds with #{@filter} filter. #{viewers} are allowed to view your story."
