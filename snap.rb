d9f6dc596077c47541a57bfbf1a87fa7a835ce41
sonyapieklik <piesonya@gmail.com>
2018-07-27 20:58:25 +0000

    automatically backed up by learn

--- snap.rb
+++ snap.rb
@@ -7,8 +7,53 @@ def initialize(filter, viewers, timer, type)
     @timer = time
     @type = type
 end
-    
-    
+
+  puts "Video or Photo on your story?"
+  type = gets.chomp
+  
+  if type == "video"
+    @type = type
+    puts "How many seconds is your video?"
+    time = gets.chomp.to_i
+    @timer = time
+    puts "Will you be using a filter? y|n"
+    answer = gets.chomp
+    if answer == "y"
+      puts "Please specify which filter you will be using (ie. dog filter, flower crown, etc.)"
+      filter = gets.chomp.downcase
+      @filter = filter 
+    else
+      @filter = "not using a"
+    end
+    puts "Who can view your story: [e]veryone or [o]nly friends"
+    view = gets.chomp
+    if view == "e"
+      @viewers = "Everyone"
+    else 
+      @viewers = "Only friends"
+    end
+  else
+        @type = type
+    puts "How many seconds is your video?"
+    time = gets.chomp.to_i
+    @timer = time
+    puts "Will you be using a filter? y|n"
+    answer = gets.chomp
+    if answer == "y"
+      puts "Please specify which filter you will be using (ie. dog filter, flower crown, etc.)"
+      filter = gets.chomp.downcase
+      @filter = filter 
+    else
+      @filter = "not using a"
+    end
+    puts "Who can view your story: [e]veryone or [o]nly friends"
+    view = gets.chomp
+    if view == "e"
+      @viewers = "Everyone"
+    else 
+      @viewers = "Only friends"
+    end
+end 
   puts "Video or Photo on your story?"
   type = gets.chomp
   if type == "video"
