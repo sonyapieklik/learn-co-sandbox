1987dfc2c755fe1fed537803780838e5bd6b4813
sonyapieklik <piesonya@gmail.com>
2018-07-27 20:37:25 +0000

    automatically backed up by learn

--- snap.rb
+++ snap.rb
@@ -19,23 +19,29 @@ def story
     time = gets.chomp.to_i
     if time < 30
       puts "It is too long! Please do below 30 seconds."
-      break
+
     else 
       @timer = time
       puts "Will you be using a filter? y|n"
       answer = gets.chomp
+    end
       if answer == "y"
         puts "Please specify which filter you will be using (ie. dog filter, flower crown, etc.)"
         filter = get.chomp.downcase
         @filter = filter 
       else 
         @filter = "not using a"
+      end
       puts "Who can view your story: [e]veryone or [o]nly friends"
       view = gets.chomp
       if view == "e"
         @viewers = "Everyone"
       else 
         @viewers = "Only friends"
-end
+      end
+  else 
+    puts "done"
+    end
+  end
 end
 puts "Your story is a #{@type} that is #{@timer} seconds with #{@filter} filter. #{viewers} are allowed to view your story."
